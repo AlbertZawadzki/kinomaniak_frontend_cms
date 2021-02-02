e00bcd2f9f9cdcdbcffc86b07883531d90f5b012
AlbertZawadzki <r3vis3dsmil3@gmail.com>
2021-02-02 11:39:26 +0100

    Fixes in env, to be fixed

--- src/components/layout/dev/index.js
+++ src/components/layout/dev/index.js
@@ -6,7 +6,7 @@ import User from "./User"
 import Tile from "../../Tile"
 
 const Dev = () => {
-  const mode = process.env.NEXT_PUBLIC_IS_DEV ? "Development" : "Production"
+  const mode = process.env.NEXT_PUBLIC_IS_DEV ? "Development" : process.env.IS_DEV ? "Development" : "Production"
 
   return (
     <Tile title={`${mode} dev box`} hidden column>
