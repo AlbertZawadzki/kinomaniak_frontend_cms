e8d637904297e97c8e023fb25c90ef863382c547
AlbertZawadzki <r3vis3dsmil3@gmail.com>
2021-03-09 14:19:38 +0100

    Added translations new file

--- src/functions/getTranslation.js
+++ src/functions/getTranslation.js
@@ -1,9 +1,24 @@
 import data from "../data/translations.json"
 
-const getTranslation = (name) => {
+const CASE_CAPITALIZE = 0
+const CASE_LOWERCASE = 1
+const CASE_UPPERCASE = 2
+
+const getTranslation = (name, casing = CASE_CAPITALIZE) => {
   name = name ? name.toString().toLowerCase() : null
 
-  return data[name] || name + " - " + data["_no_translation_available"]
+  let text = data[name] || name + " - " + data["_no_translation_available"]
+
+  switch (casing) {
+    case CASE_CAPITALIZE:
+      return text.charAt(0).toUpperCase() + text.slice(1)
+    case  CASE_LOWERCASE:
+      return text.toLowerCase()
+    case CASE_UPPERCASE:
+      return text.toUpperCase()
+    default:
+      return "[ERROR] unknown casing " + name
+  }
 }
 
 export default getTranslation
